8ad181d62c7e7c981c2decc372dfa4169343a8a9
Kye Shi <shi.kye@gmail.com>
2022-02-07 14:31:28 -0800

    hw3 statements

---
+++ 03/template/changeDP.py
@@ -0,0 +1,5 @@
+def changeDP(C, amount):
+    """
+    Takes a list of coin denominations `C` and an `amount` of change to make
+    and returns the least number of coins required to make up that amount.
+    """
---
+++ 03/template/recursive_change.py
@@ -0,0 +1,4 @@
+def recursive_change(put_the_indices_here, amount):
+    """
+    Explanatory docstring here indicates the meaning of the arguments.
+    """
